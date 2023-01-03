4591b058dc3ae9349c5d960b147a136c150a110b
simon bullado <>
2023-01-03 12:09:38 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+function sayHello(userName) {
+  console.log(`Hello, ${userName}`);
+}
+
+function sayWelcome(userName) {
+  console.log(`Welcome, ${userName}`);
+}
+
+function askUserName() {
+  const name = prompt("Hey, what's your name?");
+}
